6c2423318124920cf45d1d2b81fc1faabb97f877
Shreya Shridhar <shreyashridhar9@gmail.com>
2024-02-09 13:15:11 +0530

    fixed issue with choosing ride API

--- Cab/src/main/java/com/CabBooking/Cab/service/IFindRideService.java
+++ Cab/src/main/java/com/CabBooking/Cab/service/IFindRideService.java
@@ -8,4 +8,6 @@ import java.util.List;
 
 public interface IFindRideService {
      List<DriverAvailabilityDetailsDTO> findRide( FindRideRequest findRideRequest);
+
+     List<DriverAvailabilityDetailsDTO> getAvailableDrivers();
 }
--- Cab/src/main/java/com/CabBooking/Cab/service/impl/ChooseRideServiceImpl.java
+++ Cab/src/main/java/com/CabBooking/Cab/service/impl/ChooseRideServiceImpl.java
@@ -3,19 +3,31 @@ package com.CabBooking.Cab.service.impl;
 import com.CabBooking.Cab.representation.dto.DriverAvailabilityDetailsDTO;
 import com.CabBooking.Cab.representation.request.ChooseRideRequest;
 import com.CabBooking.Cab.service.IChooseRideService;
+import com.CabBooking.Cab.service.IFindRideService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
-import static com.CabBooking.Cab.service.impl.FindRideServiceImpl.availableDrivers;
+
+import java.util.List;
+//import static com.CabBooking.Cab.service.impl.FindRideServiceImpl.availableDrivers;
 
 @Service
+@Scope("singleton")
 public class ChooseRideServiceImpl implements IChooseRideService {
+    private final IFindRideService findRideService;
+    @Autowired
+    public ChooseRideServiceImpl(IFindRideService findRideService) {
+        this.findRideService = findRideService;
+    }
+
     @Override
     public String chooseRide(ChooseRideRequest chooseRideRequest) {
         try {
-            String toReturn = null;
+            List<DriverAvailabilityDetailsDTO> availableDrivers = findRideService.getAvailableDrivers() ;
             for (DriverAvailabilityDetailsDTO driver : availableDrivers) {
                 if (driver.getName().equals(chooseRideRequest.getDriverName()) && driver.isAvailable()) {
                     driver.setAvailable(false);
-                    return "Driver selected->" + driver.getName();
+                    return "Driver selected-> " + driver.getName();
                 }
             }
         }
